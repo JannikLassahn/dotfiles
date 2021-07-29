30bc2ec8ac71f38a5eac31b495107604c031695f
Jannik Lassahn <jannik.lassahn@gmx.de>
2021-07-29 19:31:36 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-dir = ~./dotfiles
+dir=~/.dotfiles
 
 # Remove existing files
 sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/.p10k.zsh > /dev/null 2>&1
+sudo rm -rf ~/.config/nvim > /dev/null 2>&1
 
 # Symlink new files
 ln -sf $dir/tmux/tmux.conf ~/.tmux.conf
 ln -sf $dir/zsh/.zshrc ~/.zshrc
 ln -sf $dir/zsh/.p10k.zsh ~/.p10k.zsh
+ln -sf $dir/nvim ~/.config/nvim
 
-sudo chsh -s /bin/zsh
